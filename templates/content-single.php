b56150c6e3148f699f4b966f39b91c5ffd5a4e5f
Ardi Soebrata <ardissoebrata@gmail.com>
2013-03-17 13:48:29 +0700

    Make comment textarea full-width, add page-header style to post titles.

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,6 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
 	<article <?php post_class(); ?>>
-	    <header id="post-<?php the_ID(); ?>" class="entry-header">
+	    <header id="post-<?php the_ID(); ?>" class="entry-header page-header">
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 	    </header>
 	    <div class="entry-content">
