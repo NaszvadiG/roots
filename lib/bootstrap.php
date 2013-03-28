d2225a42aff61ff9433a622be03f93fa8a59b8a3
Ardi Soebrata <ardissoebrata@gmail.com>
2013-03-28 13:29:52 +0700

    Add dropdown, hr, buttongroup, buttontoolbar, alert, well, herounit, thumbnails & tabs.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -284,6 +284,51 @@ class Beam_Bootstrap
 				'title'		=> 'Row Info',
 				'selector'	=> 'tr',
 				'classes'	=> 'info'
+			),
+			array(
+				'title'		=> 'Nav',
+				'selector'	=> 'ul',
+				'classes'	=> 'nav'
+			),
+			array(
+				'title'		=> 'Nav Tabs',
+				'selector'	=> 'ul',
+				'classes'	=> 'nav nav-tabs'
+			),
+			array(
+				'title'		=> 'Nav Pills',
+				'selector'	=> 'ul',
+				'classes'	=> 'nav nav-pills'
+			),
+			array(
+				'title'		=> 'Nav Stacked',
+				'selector'	=> 'ul',
+				'classes'	=> 'nav-stacked'
+			),
+			array(
+				'title'		=> 'Nav List',
+				'selector'	=> 'ul',
+				'classes'	=> 'nav nav-list'
+			),
+			array(
+				'title'		=> 'Nav Header',
+				'selector'	=> 'li',
+				'classes'	=> 'nav-header'
+			),
+			array(
+				'title'		=> 'Active',
+				'selector'	=> 'li',
+				'classes'	=> 'active'
+			),
+			array(
+				'title'		=> 'Disabled',
+				'selector'	=> 'li',
+				'classes'	=> 'disabled'
+			),
+			array(
+				'title'		=> 'Divider',
+				'selector'	=> 'li',
+				'classes'	=> 'divider'
 			)
 		);
 		$settings['style_formats'] = json_encode($style_formats);
@@ -304,19 +349,26 @@ class Beam_Bootstrap
 		add_shortcode('dl', array( $this, 'bs_dl' ));
 		add_shortcode('dt', array( $this, 'bs_dt' ));
 		add_shortcode('dd', array( $this, 'bs_dd' ));
-		add_shortcode('button', array( $this, 'bs_button' ));    
-//		add_shortcode('alert', array( $this, 'bs_alert' ));
+		add_shortcode('button', array( $this, 'bs_button' ));  
+		add_shortcode('dropdown', array( $this, 'bs_dropdown' ));
+		add_shortcode('hr', array( $this, 'bs_hr' ));
+		add_shortcode('buttongroup', array( $this, 'bs_buttongroup' ));
+		add_shortcode('buttontoolbar', array( $this, 'bs_buttontoolbar' ));
+		add_shortcode('alert', array( $this, 'bs_alert' ));
 //		add_shortcode('code', array( $this, 'bs_code' ));
-//		add_shortcode('label', array( $this, 'bs_label' ));
-//		add_shortcode('badge', array( $this, 'bs_badge' ));
+		add_shortcode('label', array( $this, 'bs_label' ));
+		add_shortcode('badge', array( $this, 'bs_badge' ));
 		add_shortcode('icon', array( $this, 'bs_icon' ));
 		add_shortcode('icon_white', array( $this, 'bs_icon_white' ));
 //		add_shortcode('table', array( $this, 'bs_table' ));
 //		add_shortcode('collapsibles', array( $this, 'bs_collapsibles' ));
 //		add_shortcode('collapse', array( $this, 'bs_collapse' ));
-//		add_shortcode('well', array( $this, 'bs_well' ));
-//		add_shortcode('tabs', array( $this, 'bs_tabs' ));
-//		add_shortcode('tab', array( $this, 'bs_tab' ));
+		add_shortcode('well', array( $this, 'bs_well' ));
+		add_shortcode('herounit', array( $this, 'bs_herounit' ));
+		add_shortcode('thumbnails', array( $this, 'bs_thumbnails' ));
+		add_shortcode('thumbnail', array( $this, 'bs_thumbnail' ));
+		add_shortcode('tabs', array( $this, 'bs_tabs' ));
+		add_shortcode('tab', array( $this, 'bs_tab' ));
 		add_shortcode('holder', array( $this, 'bs_holder' ));
 	}
 	
@@ -454,6 +506,350 @@ class Beam_Bootstrap
 	}
 	
 	/**
+	 * Add dropdown menu.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_dropdown( $atts, $content = null ) {
+		extract(shortcode_atts(array(
+			'display' => '',
+			'dropup' => ''
+		), $atts));
+		
+		$dom = new DOMDocument;
+		$dom->loadHTML($content);
+		$content = $this->create_dropdown($dom, $atts);
+		
+		$class = '';
+		if (!empty($dropup)) {
+			$class = 'dropup';
+		} else {
+			$class = 'dropdown';
+		}
+		if (!empty($display)) {
+			$class .= ' clearfix';
+		}
+		return '<div class="' . $class . '">' . do_shortcode( $content ) . '</div>';
+	}
+	
+	/**
+	 * Display hr.
+	 * 
+	 * @return string
+	 */
+	function bs_hr() {
+		return '<hr />';
+	}
+	
+	/**
+	 * Create button groups.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_buttongroup( $atts, $content = null ) {
+		extract(shortcode_atts(array(
+			'vertical' => '',
+			'dropup' => ''
+		), $atts));
+		
+		$dom = new DOMDocument;
+		$dom->loadHTML($content);
+		$xpath = new DOMXPath($dom);
+		$class = 'btn-group';
+		
+		if ( $xpath->query('//ul')->length > 0) {
+			$content = do_shortcode( $this->create_dropdown($dom, $atts) );
+			if (!empty($dropup)) {
+				$class .= ' dropup';
+			}
+		} else {
+			if (!empty($vertical)) {
+				$class .= ' btn-group-vertical';
+			}
+		}
+		return '<div class="' . $class . '">' . do_shortcode($content) . '</div>';
+	}
+	
+	/**
+	 * Create button toolbar.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_buttontoolbar( $atts, $content = null ) {
+		return '<div class="btn-toolbar">' . do_shortcode($content) . '</div>';
+	}
+	
+	/**
+	 * Create dropdown component.
+	 * 
+	 * @param DOMDocument $dom
+	 * @param string $display
+	 * @return string
+	 */
+	function create_dropdown( $dom, $atts ) {
+		extract(shortcode_atts(array(
+			'display' => '',
+			'split' => '',
+			'dropup' => '',
+			'left' => '',
+			'right' => ''
+		), $atts));
+		
+		$xpath = new DOMXPath($dom);
+		
+		// Format dropdown-toggle.
+		if (empty($split)) {
+			foreach ($xpath->query('/html/body/a') as $node) {
+				$this->add_class($node, 'dropdown-toggle');
+				$node->setAttribute('data-toggle', 'dropdown');
+				$node->nodeValue .= ' ';
+				$caret = $dom->createElement('span');
+				$caret->setAttribute('class', 'caret');
+				$node->appendChild($caret);
+			}
+		} else {
+			foreach ($xpath->query('/html/body/a') as $node_a) {
+				$class = $node_a->getAttribute('class');
+				$uls = $xpath->query('/html/body/ul');
+				if ($uls->length > 0) {
+					$node_ul = $uls->item(0);
+					$split_btn = $dom->createElement('a');
+					$split_btn->setAttribute('class', $class . ' dropdown-toggle');
+					$split_btn->setAttribute('data-toggle', 'dropdown');
+					$caret = $dom->createElement('span');
+					$caret->setAttribute('class', 'caret');
+					$split_btn->appendChild($caret);
+					$xpath->query('/html/body')->item(0)->insertBefore($split_btn, $node_ul);
+				}
+			}
+		}
+
+		// Add .dropdown-submenu class
+		foreach ($xpath->query('//li[ul]') as $ulnode) {
+			$class = 'dropdown-submenu';
+			if (!empty($left)) $class .= ' pull-left';
+			$this->add_class($ulnode, $class);
+		}
+
+		// Add .dropdown-menu
+		foreach ($xpath->query('//ul') as $ulnode) {
+			$class = 'dropdown-menu';
+			if (!empty($right)) $class .= ' pull-right';
+			$this->add_class($ulnode, $class);
+			
+			if ($ulnode->parentNode->nodeName != 'li') {
+				$ulnode->setAttribute('role', 'menu');
+				$ulnode->setAttribute('aria-labelledby', 'dlabel');
+
+				if (!empty($display)) {
+					$ulnode->setAttribute('style', 'display: block; position: static;');
+				}
+			}
+		}
+
+		// Add .divider class to empty li.
+		foreach ($xpath->query('//li[not(node())]') as $linode) {
+			$this->add_class($linode, 'divider');
+		}
+
+		$this->remove_nodes($dom, 'p');
+		$this->remove_nodes($dom, 'br');
+
+		$content = preg_replace('~<(?:!DOCTYPE|/?(?:html|head|body))[^>]*>\s*~i', '', $dom->saveHTML());
+		return str_replace("\r\n", "", $content);
+	}
+	
+	/**
+	 * Add class to node element.
+	 * 
+	 * @param DOMNode $node
+	 * @param string $class
+	 */
+	function add_class($node, $class) {
+		$curr_class = '';
+		if ($node->hasAttribute('class')) {
+			$curr_class = $node->getAttribute('class') . ' ';
+		}
+		$curr_class .= $class;
+		$node->setAttribute('class', $curr_class);
+	}
+	
+	/**
+	 * Remove empty elements.
+	 * 
+	 * @param DOMDocument $dom
+	 * @param string $name
+	 */
+	function remove_nodes($dom, $name) {
+		$xpath = new DOMXPath($dom);
+		$deletenodes = array();
+		foreach ($xpath->query('//' . $name . '[not(node())]') as $node) {
+			$deletenodes[] = $node;
+		}
+		foreach ($deletenodes as $node) {
+			$node->parentNode->removeChild($node);
+		}
+	}
+	
+	/**
+	 * Display alert.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_alert( $atts, $content = null ) {
+		extract(shortcode_atts(array(
+			'type'	=> '',
+			'block'	=> 'false',
+			'close'	=> 'true'
+		), $atts));
+		
+		$class = 'alert';
+		if (!empty($type))
+			$class .= ' alert-' . $type;
+		if ($block == 'true')
+			$class .= ' alert-block';
+		
+		$output = '<div class="' . $class . '">';
+		if ($close == 'true')
+			$output .= '<button type="button" class="close" data-dismiss="alert">&times;</button>';
+		$output .= trim(do_shortcode($content));
+		$output .= '</div>';
+		return $output;
+	}
+	
+	/**
+	 * Apply well to elements.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_well( $atts, $content = '' ) {
+		return '<div class="well">' . do_shortcode($content) . '</div>';
+	}
+	
+	/**
+	 * Apply hero unit to elements.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_herounit( $atts, $content = '' ) {
+		return '<div class="hero-unit">' . do_shortcode($content) . '</div>';
+	}
+	
+	/**
+	 * Apply thumbnails to elements.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_thumbnails( $atts, $content = '' ) {
+		return '<div class="row-fluid"><ul class="thumbnails">' . do_shortcode($content) . '</ul></div>';
+	}
+	
+	/**
+	 * Apply thumbnail to elements.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_thumbnail( $atts, $content = '' ) {
+		extract(shortcode_atts(array(
+			'span'	=> '',
+			'strip'	=> '',
+		), $atts));
+		$class = '';
+		if (!empty($span)) {
+			$class .= ' class="span' . $span . '"';
+		}
+		if (!empty($strip)) {
+			$content = str_replace("\r\n", "", $content);
+		}
+		return '<li' . $class . '><div class="thumbnail">' . do_shortcode(trim($content)) . '</div></li>';
+	}
+	
+	/**
+	 * Display tabs
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_tabs( $atts, $content = '' ) {
+		extract(shortcode_atts(array(
+			'position'	=> ''
+		), $atts));
+		
+		if (!isset($GLOBALS['tab-id']))
+			$GLOBALS['tab-id'] = 1;
+		else
+			$GLOBALS['tab-id']++;
+		
+		$GLOBALS['tabs'] = array();
+		$GLOBALS['tab-active'] = '';
+		$content = do_shortcode($content);
+		
+		if (count($GLOBALS['tabs']) == 0) {
+			return $content;
+		}
+		$content = '<div class="tab-content">' . $content . '</div>';
+		
+		$tabs = '';
+		foreach ($GLOBALS['tabs'] as $slug => $title) {
+			$activate = '';
+			if ($GLOBALS['tab-active'] == $slug) $activate = ' class="active"';
+			$tabs .= '<li' . $activate . '><a href="#' . $slug . '" data-toggle="tab">' . $title . '</a></li>';
+		}
+		$tabs = '<ul class="nav nav-tabs">' . $tabs . '</ul>';
+		
+		$class = 'tabbable';
+		if (!empty($position)) {
+			$position = strtolower($position);
+			$class .= ' tabs-' . $position;
+		}
+		if ($position == 'below')
+			return '<div class="' . $class . '">' . $content . $tabs . '</div>';
+		else
+			return '<div class="' . $class . '">' . $tabs . $content . '</div>';
+	}
+	
+	/**
+	 * Apply tab to elements.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_tab( $atts, $content = '' ) {
+		extract(shortcode_atts(array(
+			'title'		=> 'Tab',
+			'active'	=> 'false'
+		), $atts));
+		$slug = 'tab-' . $GLOBALS['tab-id'] . '-' . sanitize_title( $title );
+		$GLOBALS['tabs'][$slug] = $title;
+		
+		$class = 'tab-pane';
+		if (strtoupper($active) == 'TRUE') {
+			$GLOBALS['tab-active'] = $slug;
+			$class .= ' active';
+		}
+		
+		return '<div id="' . $slug . '" class="' . $class . '">' . do_shortcode($content) . '</div>';
+	}
+	
+	/**
 	 * Display icons.
 	 * 
 	 * $atts params:
@@ -480,13 +876,42 @@ class Beam_Bootstrap
 	 * @param array $atts
 	 * @return string
 	 */
-	function bs_icon_white( $atts )
-	{
+	function bs_icon_white( $atts )	{
 		extract(shortcode_atts(array(
 			'type' => '',
 		), $atts));
 		
 		return '<i class="icon icon-' . $type . ' icon-white"></i>';
+	}
+	
+	/**
+	 * Display labels.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_label( $atts, $content = '' ) {
+		extract(shortcode_atts(array(
+			'type' => '',
+		), $atts));
+		
+		return '<span class="label label-' . $type . '">' . do_shortcode($content) . '</span>';
+	}
+	
+	/**
+	 * Display badges.
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	function bs_badge( $atts, $content = '' ) {
+		extract(shortcode_atts(array(
+			'type' => '',
+		), $atts));
+		
+		return '<span class="badge badge-' . $type . '">' . do_shortcode($content) . '</span>';
 	}
 	
 	/**
